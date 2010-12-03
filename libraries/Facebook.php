d74b3b43e58c4e09adf04f4d1469fe8874bdd0fd
Jaisen Mathai <jaisen@jmathai.com>
2010-12-03 00:28:35 -0800

    Resolving name conflict for facebook files.

--- libraries/Facebook.php
+++ libraries/Facebook.php
@@ -1,5 +1,5 @@
 <?php
-class FacebookPhotos
+class Facebook
 {
   public static function getAlbums($token, $uid)
   {
@@ -7,8 +7,22 @@ class FacebookPhotos
     $albums = getFacebook()->api("/{$uid}/albums", 'GET', array('access_token' => $token));
     foreach($albums['data'] as $album)
     {
-      $retval[] = array('id' => $album['id'], 'name' => $album['name'], 'cover' => "/proxy/r/facebook/{$album['id']}/picture");
+      $retval[] = array('id' => $album['id'], 'name' => $album['name'], 'cover' => "/proxy/r/facebook/{$album['id']}/picture",
+        'link' => '/proxy/p/'.Credential::serviceFacebook."/?id={$album['id']}&method=photos");
     }
     return $retval;
   }
+
+  public static function getPhotos($token, $uid)
+  {
+    $retval = array();
+    $photos = getFacebook()->api("/{$uid}/photos", 'GET', array('access_token' => $token));
+    foreach($photos['data'] as $photo)
+    {
+      $retval[] = array('id' => $photo['id'], 'title' => $photo['Caption'], 
+        'thumbUrl' => $photo['picture'], 'mediumUrl' => null,
+        'originalUrl' => $photo['source']);
+    }
+    return $retval;
+ }
 }
