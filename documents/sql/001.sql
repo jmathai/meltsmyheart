87121806eae9892296a160c65069ea2980150f11
Jaisen Mathai <jaisen@jmathai.com>
2010-12-04 02:21:34 -0800

    Sql updates including photo table.

--- documents/sql/001.sql
+++ documents/sql/001.sql
@@ -1,3 +1,33 @@
+CREATE TABLE IF NOT EXISTS `child` (
+  `c_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `c_u_id` int(10) unsigned NOT NULL,
+  `c_name` varchar(255) NOT NULL,
+  `c_birthdate` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`c_id`),
+  KEY `c_u_id` (`c_u_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `credential` (
+  `c_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `c_u_id` int(10) unsigned NOT NULL,
+  `c_service` enum('facebook','smugmug') NOT NULL,
+  `c_token` varchar(255) NOT NULL,
+  `c_secret` varchar(255) DEFAULT NULL,
+  `c_uid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`c_id`),
+  UNIQUE KEY `userToService` (`c_u_id`,`c_service`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `photo` (
+  `p_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `p_u_id` int(10) unsigned NOT NULL,
+  `p_key` varchar(255) NOT NULL,
+  `p_meta` text NOT NULL,
+  `p_dateCreated` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`p_id`),
+  UNIQUE KEY `userId_key` (`p_u_id`,`p_key`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `user` (
   `u_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `u_key` char(32) NOT NULL,
@@ -9,12 +39,3 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`u_id`),
   UNIQUE KEY `u_email` (`u_email`,`u_password`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `credential` (
-  `c_u_id` int(10) unsigned NOT NULL,
-  `c_service` enum('facebook','smugmug') NOT NULL,
-  `c_token` varchar(255) NOT NULL,
-  `c_secret` varchar(255) NOT NULL,
-  PRIMARY KEY (`c_u_id`,`c_service`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-ALTER TABLE  `credential` CHANGE  `c_secret`  `c_secret` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
