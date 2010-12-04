53d33e050db2dc4246d8519ddd36e87593fdc354
Jaisen Mathai <jaisen@jmathai.com>
2010-12-03 23:21:39 -0800

    Adding initial NoSql class

---
+++ libraries/NoSql.php
@@ -0,0 +1,28 @@
+<?php
+class NoSql
+{
+  public static function add($userId, $key, $value)
+  {
+
+  }
+
+  public static function all($userId, $key)
+  {
+    $retval = getDatabase()->all('SELECT * FROM json WHERE j_u_id
+  }
+
+  public static function deleteOne($userId, $id)
+  {
+
+  }
+
+  public static function deleteAll($userId, $key)
+  {
+
+  }
+
+  public static function one($userId, $key)
+  {
+
+  }
+}
