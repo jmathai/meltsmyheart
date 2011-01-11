9f2efdd296ebbc8ceb925d2e1939e361cacb6dda
Jaisen Mathai <jaisen@jmathai.com>
2011-01-11 00:21:06 -0800

    Adding css for header with sprite.

--- views/template.php
+++ views/template.php
@@ -4,17 +4,27 @@
   <link rel="stylesheet" type="text/css" href="/css/styles.css">
 </head>
 <body>
-  <ul id="navigation">
-    <li><a href="/">Home</a></li>
-    <li><a href="/child/new">New Child</a></li>
-    <li><a href="/login">Login</a> or <a href="/join">join</a></li>
-    <li><a href="/upgrade">Upgrade</a></li>
-    <li><a href="/logout">Logout</a></li>
-  </ul>
-  <?php include $body; ?>
-  <ul>
-    <li>Footer goes here</li.
-  </ul>
+  <div id="header">
+    <div class="container">
+      <a href="/" title="<?php echo getConfig()->get('site')->name; ?>"><img src="/img/logo.png" id="logo" alt="<?php echo getConfig()->get('site')->name; ?>"></a>
+      <ul id="navigation">
+        <li><a href="/child/new" class="add-child"><div></div>Add Child</a></li>
+        <li><a href="/share" class="share"><div></div>Share</a></li>
+        <li><a href="/upgrade" class="upgrade"><div></div>Upgrade</a></li>
+        <!--<li><a href="/login">Login</a> or <a href="/join">join</a></li>-->
+        <!--<li><a href="/logout">Logout</a></li>-->
+      </ul>
+    </div>
+  </div>
+  <div id="header-bar"></div>
+  <div id="content" class="container">
+    <?php include $body; ?>
+  </div>
+  <div id="footer" class="container">
+    <ul>
+      <li>Footer goes here</li.
+    </ul>
+  </div>
   <script src="/js/jquery.js"></script>
   <script src="/js/swfupload.js"></script>
   <script src="/js/plugins/swfupload.queue.js"></script>
