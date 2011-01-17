79948977debe6c175d451784a53481a593006e28
Jaisen Mathai <jaisen@jmathai.com>
2011-01-17 00:20:27 -0800

    Styling the share page.

--- views/share.php
+++ views/share.php
@@ -1,8 +1,11 @@
 <h1>Share with friends and family</h1>
 
+<p>
+  Easily share with your friends on Facebook. Just click the child's name you'd like to share.
+</p>
+
 <?php foreach($children as $child) { ?>
   <p>
-    <h2>Share <?php echo posessive($child['c_name']); ?> page</h2>
-    <a href="/share/facebook/<?php echo $child['c_id']; ?>" class="facebook overlay_target" rel="#modal"></a>
+  <a href="/share/facebook/<?php echo $child['c_id']; ?>" class="overlay_target" rel="#modal"><button><div><?php echo $child['c_name']; ?></div></button></a>
   </p>
 <?php } ?>
