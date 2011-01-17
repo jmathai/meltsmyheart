df88e062670e34c39acf7534b4e53316bab584ad
Jaisen Mathai <jaisen@jmathai.com>
2011-01-17 00:29:41 -0800

    Fixing child page photo urls.

--- views/childPage.php
+++ views/childPage.php
@@ -1,8 +1,8 @@
 <h1><?php echo $child['c_name']; ?><h1>
 <h3>Born <?php echo date('l jS \of F', $child['c_birthdate']); ?></h3>
 <?php foreach($photos as $week => $weekPhotos) { ?>
-  <h5><?php echo $child['c_name']; ?> week <?php echo $week; ?></h5>
+  <h5><?php echo sprintf('%s %s', $child['c_name'], $week); ?></h5>
   <?php foreach($weekPhotos as $photo) { ?>
-    <img src="/photos<?php echo '/photos'.Photo::generateUrl($photo['p_basePath'], 200, 200/*, array(Photo::contrast)*/); ?>" hspace="5" vspace="5" title="<?php echo date('Y-m-d', $photo['p_dateTaken']); ?>">
+    <img src="/photos<?php echo Photo::generateUrl($photo['p_basePath'], 200, 200/*, array(Photo::contrast)*/); ?>" hspace="5" vspace="5" title="<?php echo date('Y-m-d', $photo['p_dateTaken']); ?>">
   <?php } ?>
 <?php } ?>
