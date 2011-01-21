ee70c22a5572a535be21ca7c94995bf9e764967e
Jaisen Mathai <jaisen@jmathai.com>
2011-01-21 12:18:15 -0800

    Fixing 'www' subdomain issue. Updating form validation error on child new page for birthdate format.

--- html/index.php
+++ html/index.php
@@ -7,7 +7,7 @@ include getConfig()->get('paths')->controllers . '/Api.php';
 
 // routes
 getRoute()->load('routes.ini');
-if(preg_match('/^([a-zA-Z0-9-]+)\.meltsmyheart\.com$/', $_SERVER['HTTP_HOST'], $matches))
+if(preg_match('/^([a-zA-Z0-9-]+)\.meltsmyheart\.com$/', $_SERVER['HTTP_HOST'], $matches) && isset($matches[1]) && $matches[1] != 'www')
   getRoute()->run("/child/{$matches[1]}"); 
 else
   getRoute()->run(); 
