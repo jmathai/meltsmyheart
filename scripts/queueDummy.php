22e0c504258470e93359d2e41cc7e92b349f3e0b
Jaisen Mathai <jaisen@jmathai.com>
2011-01-23 17:41:46 -0800

    Reverting persistent connection and adding chdir to queueDummy.

--- scripts/queueDummy.php
+++ scripts/queueDummy.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: text/css');
+chdir(dirname(__FILE__));
 ini_set('include_path', '.');
 require '../configs/init.php';
 Resque::enqueue('mmh_dummy', 'QueueDummy', array());
